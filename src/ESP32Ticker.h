c2e265a5c7875725f6af46606cb8a0d06cf58a2e
Bert Melis <bertmelis@users.noreply.github.com>
2018-01-12 09:59:34 +0100

    Bugfix MAX_NUMBER_TICKERS
    * correctly use MAX_NUMBER_TICKERS
    * use cpplint

--- src/ESP32Ticker.h
+++ src/ESP32Ticker.h
@@ -1,11 +1,11 @@
 /* 
   ESP32Ticker.h - esp32 library that calls functions periodically
 
-	Copyright (c) 2017 Bert Melis. All rights reserved.
-	
-	Based on the original work of:
+  Copyright (c) 2017 Bert Melis. All rights reserved.
+  
+  Based on the original work of:
   Copyright (c) 2014 Ivan Grokhotkov. All rights reserved.
-	The original version is part of the esp8266 core for Arduino environment.
+  The original version is part of the esp8266 core for Arduino environment.
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -23,75 +23,75 @@
 */
 #pragma once
 
-#define MAX_NUMER_TICKERS 10
+#define MAX_NUMBER_TICKERS 10
 
 extern "C" {
-	#include "freertos/FreeRTOS.h"
-	#include "freertos/timers.h"
+  #include "freertos/FreeRTOS.h"
+  #include "freertos/timers.h"
 }
 
 class Ticker {
-public:
+ public:
   Ticker();
-	~Ticker();
-	typedef void (*callback_t)(void); 
-	typedef void (*callback_with_arg_t)(void*); 
+  ~Ticker();
+  typedef void (*callback_t)(void);
+  typedef void (*callback_with_arg_t)(void*);
 
-	void attach(float seconds, callback_t callback) {
-		_attach_ms(seconds * 1000, true, reinterpret_cast<callback_with_arg_t>(callback), 0);
-	}
-	
-	void attach_ms(uint32_t milliseconds, callback_t callback) {
-		_attach_ms(milliseconds, true, reinterpret_cast<callback_with_arg_t>(callback), 0);
-	}
-	
-	template<typename TArg>
-	void attach(float seconds, void (*callback)(TArg), TArg arg) {
-		static_assert(sizeof(TArg) <= sizeof(uint32_t), "attach() callback argument size must be <= 4 bytes");
-		// C-cast serves two purposes:
-		// static_cast for smaller integer types,
-		// reinterpret_cast + const_cast for pointer types
-		uint32_t arg32 = (uint32_t)arg;
-		_attach_ms(seconds * 1000, true, reinterpret_cast<callback_with_arg_t>(callback), arg32);
-	}
+  void attach(float seconds, callback_t callback) {
+    _attach_ms(seconds * 1000, true, reinterpret_cast<callback_with_arg_t>(callback), 0);
+  }
 
-	template<typename TArg>
-	void attach_ms(uint32_t milliseconds, void (*callback)(TArg), TArg arg) {
-	static_assert(sizeof(TArg) <= sizeof(uint32_t), "attach_ms() callback argument size must be <= 4 bytes");
-	uint32_t arg32 = (uint32_t)arg;
-	_attach_ms(milliseconds, true, reinterpret_cast<callback_with_arg_t>(callback), arg32);
-	}
+  void attach_ms(uint32_t milliseconds, callback_t callback) {
+    _attach_ms(milliseconds, true, reinterpret_cast<callback_with_arg_t>(callback), 0);
+  }
 
-	void once(float seconds, callback_t callback) {
-		_attach_ms(seconds * 1000, false, reinterpret_cast<callback_with_arg_t>(callback), 0);
-	}
+  template<typename TArg>
+  void attach(float seconds, void (*callback)(TArg), TArg arg) {
+    static_assert(sizeof(TArg) <= sizeof(uint32_t), "attach() callback argument size must be <= 4 bytes");
+    // C-cast serves two purposes:
+    // static_cast for smaller integer types,
+    // reinterpret_cast + const_cast for pointer types
+    uint32_t arg32 = (uint32_t)arg;
+    _attach_ms(seconds * 1000, true, reinterpret_cast<callback_with_arg_t>(callback), arg32);
+  }
 
-	void once_ms(uint32_t milliseconds, callback_t callback) {
-		_attach_ms(milliseconds, false, reinterpret_cast<callback_with_arg_t>(callback), 0);
-	}
+  template<typename TArg>
+  void attach_ms(uint32_t milliseconds, void (*callback)(TArg), TArg arg) {
+  static_assert(sizeof(TArg) <= sizeof(uint32_t), "attach_ms() callback argument size must be <= 4 bytes");
+  uint32_t arg32 = (uint32_t)arg;
+  _attach_ms(milliseconds, true, reinterpret_cast<callback_with_arg_t>(callback), arg32);
+  }
 
-	template<typename TArg>
-	void once(float seconds, void (*callback)(TArg), TArg arg) {
-		static_assert(sizeof(TArg) <= sizeof(uint32_t), "attach() callback argument size must be <= 4 bytes");
-		uint32_t arg32 = (uint32_t)(arg);
-		_attach_ms(seconds * 1000, false, reinterpret_cast<callback_with_arg_t>(callback), arg32);
-	}
+  void once(float seconds, callback_t callback) {
+    _attach_ms(seconds * 1000, false, reinterpret_cast<callback_with_arg_t>(callback), 0);
+  }
 
-	template<typename TArg>
-	void once_ms(uint32_t milliseconds, void (*callback)(TArg), TArg arg) {
-		static_assert(sizeof(TArg) <= sizeof(uint32_t), "attach_ms() callback argument size must be <= 4 bytes");
-		uint32_t arg32 = (uint32_t)(arg);
-		_attach_ms(milliseconds, false, reinterpret_cast<callback_with_arg_t>(callback), arg32);
-	}
+  void once_ms(uint32_t milliseconds, callback_t callback) {
+    _attach_ms(milliseconds, false, reinterpret_cast<callback_with_arg_t>(callback), 0);
+  }
 
-	void detach();
+  template<typename TArg>
+  void once(float seconds, void (*callback)(TArg), TArg arg) {
+    static_assert(sizeof(TArg) <= sizeof(uint32_t), "attach() callback argument size must be <= 4 bytes");
+    uint32_t arg32 = (uint32_t)(arg);
+    _attach_ms(seconds * 1000, false, reinterpret_cast<callback_with_arg_t>(callback), arg32);
+  }
 
-	static void tickerCallback(TimerHandle_t timerHandle);
+  template<typename TArg>
+  void once_ms(uint32_t milliseconds, void (*callback)(TArg), TArg arg) {
+    static_assert(sizeof(TArg) <= sizeof(uint32_t), "attach_ms() callback argument size must be <= 4 bytes");
+    uint32_t arg32 = (uint32_t)(arg);
+    _attach_ms(milliseconds, false, reinterpret_cast<callback_with_arg_t>(callback), arg32);
+  }
 
-protected:
-	void _attach_ms(uint32_t milliseconds, bool repeat, callback_with_arg_t callback, uint32_t arg);
-	static Ticker* _timers[MAX_NUMER_TICKERS];
-	TimerHandle_t _timerHandle;
-	callback_with_arg_t _callback;
-	uint32_t _arg;
+  void detach();
+
+  static void tickerCallback(TimerHandle_t timerHandle);
+
+ protected:
+  void _attach_ms(uint32_t milliseconds, bool repeat, callback_with_arg_t callback, uint32_t arg);
+  static Ticker* _timers[MAX_NUMBER_TICKERS];
+  TimerHandle_t _timerHandle;
+  callback_with_arg_t _callback;
+  uint32_t _arg;
 };
